911647575df177a92c76a1e340355a645efe81fa
memoTux <romeo@mendezfuentes.net>
2023-09-21 15:45:52 -0600

    add GQL schema

---
+++ server/schema.ts
@@ -0,0 +1,22 @@
+// https://github.com/graphql/graphql-http#create-a-graphql-schema
+
+import { GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql';
+
+/**
+ * Construct a GraphQL schema and define the necessary resolvers.
+ *
+ * type Query {
+ *   hello: String
+ * }
+ */
+export const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve: () => 'world',
+      },
+    },
+  }),
+});
