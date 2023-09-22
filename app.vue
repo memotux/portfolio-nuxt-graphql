ffa09faf72bf6a361c9eec12efdc95ce293e4f45
memoTux <romeo@mendezfuentes.net>
2023-09-22 09:16:21 -0600

    add nuxt apollo client

--- app.vue
+++ app.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+const { data } = await useAsyncQuery(
+  gql`
+    query {
+      hello
+    }
+  `
+)
+
+console.log(data.value)
+</script>
+
 <template>
   <div>
     <NuxtWelcome />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/apollo"],
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint: 'http://localhost:3000/api/graphql'
+      }
+    },
+  },
 })
